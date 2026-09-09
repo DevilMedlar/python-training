bd0e5651b7e63c1e34a2114916e3a8b70c94b68d
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-09-09 06:56:34 +0000

    3rd reading_goal

--- reading_goals.py
+++ reading_goals.py
@@ -2,13 +2,16 @@ days = int(input("How many days did you spend reading? "))
 completed_books = int(input(f"How many books did you read in {days} day(s)? "))
 both_goals_reached = completed_books >= 3 and days >= 5
 
-if both_goals_reached:
-    print("Both goals reached")
-elif completed_books >= 3 or days >= 5:
-    print("One goal reached")
+if completed_books < 0 or days < 0:
+    print("Counts cannot be negative.")
 else:
-    print("Neither goal reached")
+    if both_goals_reached:
+        print("Both goals reached")
+    elif completed_books >= 3 or days >= 5:
+        print("One goal reached")
+    else:
+        print("Neither goal reached")
 
-if not both_goals_reached:
-    print("Keep workning on the remaining goals.")
-    print(f"Progress: {completed_books} over {days} day(s)")
+    if not both_goals_reached:
+        print("Keep working on the remaining goals.")
+        print(f"Progress: {completed_books} books over {days} day(s)")
