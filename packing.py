d27b3e615d3de43474460d8b4d8e57b9f7ef9c8d
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-09-09 03:53:17 +0000

    2nd packing

--- packing.py
+++ packing.py
@@ -2,9 +2,9 @@ stickers = 26
 pack_size = 4
 full_packs = stickers // pack_size
 leftover_stickers = stickers % pack_size
-packed_stickers = stickers - leftover_stickers
+packed_stickers = pack_size * full_packs
 division_result = stickers / pack_size
+print(full_packs)
 print(leftover_stickers)
 print(packed_stickers)
-print(full_packs)
 print(division_result)
