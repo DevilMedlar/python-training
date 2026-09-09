012b76cbc107b116008b2882c646a048ee7178d5
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-09-09 04:13:08 +0000

    1st garden

---
+++ garden.py
@@ -0,0 +1,9 @@
+side_length = 5
+extension = 2
+
+original_area = side_length ** 2
+expanded_area = (side_length + extension) ** 2
+added_area = expanded_area - original_area
+print(original_area)
+print(expanded_area)
+print(added_area)
