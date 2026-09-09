c89b85b399476244c1d0251539a1ac6f5e683a60
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-09-09 05:22:00 +0000

    1st comparison

---
+++ comparisons.py
@@ -0,0 +1,9 @@
+target = 20
+
+books = int(input("How many books do you have? "))
+print(target == books)
+print(target != books)
+print(target < books)
+print(target <= books)
+print(target > books)
+print(target >= books)
